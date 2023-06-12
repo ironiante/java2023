231c31aef6644607f9071b13cbdedff2b0c8434a
jirma <jirman.rodriguez@uptc.edu.co>
2023-06-12 10:28:12 -0500

    ejercicio variables

---
+++ src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        // segundo ejemplo con variables
+        String nombre= "lucas";
+        Integer nroEquipo=1;
+        String texto = "hola soy "  + nombre + ", del equipo nro." + nroEquipo;
+        System.out.println(texto);
+
+    }
+}
