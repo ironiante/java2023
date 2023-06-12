3845e06854fc18f9aff1e91e9e39d784c5db3b10
jirma <jirman.rodriguez@uptc.edu.co>
2023-06-12 11:43:29 -0500

    OperadoresDeAsignacion

---
+++ src/OperadoresDeAsignacion.java
@@ -0,0 +1,17 @@
+public class OperadoresDeAsignacion {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        System.out.println("Valor original de a: " + a);
+        System.out.println("Valor original de b: " + b);
+
+        // Operadores de asignación
+        a += 2; // a = a + 2;
+        b *= 4; // b = b * 4;
+
+        System.out.println("Valor de a después de la asignación: " + a);
+        System.out.println("Valor de b después de la asignación: " + b);
+    }
+}
